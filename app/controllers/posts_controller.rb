f086d0a171c3a98c7255093f96665e4ca4085c71
DurandA <arnaud.durand@unifr.ch>
2013-09-29 15:17:02 +0200

    Fixed posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,13 @@
 class PostsController < ApplicationController
+
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
   def index
     @posts = Post.all
   end
 
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(params[:post].permit(:title, :text))
-
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
+  def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -26,28 +17,37 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    if @post.update(params[:post].permit(:title, :text))
-      redirect_to @post
+    if @post.update(post_params)
+      redirect_to action: :show, id: @post.id
     else
       render 'edit'
     end
   end
 
-  private
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def new
+    @post = Post.new
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to action: :show, id: @post.id
+    else
+      render 'new'
+    end
+  end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
 
-    redirect_to posts_path
+    redirect_to action: :index
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
