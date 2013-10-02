e36b2f7ba662b942a24e89c938fbb7634d758faf
DurandA <arnaud.durand@unifr.ch>
2013-10-02 14:41:27 +0200

    Added bid validation

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,12 @@
 class Bid < ActiveRecord::Base
   belongs_to :car
-  validates :amount, presence: true
+  validates :amount, presence: true,
+  numericality: {only_integer: true}
+  validate :bid_amount
+
+  def bid_amount
+    if Bid.exists? ['amount >= ?', amount]
+      errors.add( :amount, 'bids cannot be lower or equal to the previous one')
+    end
+  end
 end
