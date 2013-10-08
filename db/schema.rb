01cc1fab27732152efad2d7c55a42321192fb44f
DurandA <arnaud.durand@unifr.ch>
2013-10-08 13:32:03 +0200

    Removed signed-in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131004112931) do
+ActiveRecord::Schema.define(version: 20131004193413) do
 
   create_table "bids", force: true do |t|
     t.integer  "amount"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20131004112931) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
 
 end
